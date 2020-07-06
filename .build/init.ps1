e1607872707292f83f46c0c2c983c2245c4450c1
Steve Brown <steve.brown@thebrownnet.co.uk>
2020-07-06 10:56:47 +0100

    missing dot

--- .build/init.ps1
+++ .build/init.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 # Ensure our dependencies are present
-"$PSScriptRoot\dependencies.ps1"
+. "$PSScriptRoot\dependencies.ps1"
 
 # Import the provisioning module
 Import-Module "$PSScriptRoot\Provisioning\Provisioning.psm1"
