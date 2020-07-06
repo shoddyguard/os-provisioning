873f3c0a655a9102a5e5295e424a68c442fb0ac5
Steve Brown <steve.brown@thebrownnet.co.uk>
2020-07-06 01:56:42 +0100

    slight tidy-up

--- .build/dependencies.ps1
+++ .build/dependencies.ps1
@@ -1,27 +1,19 @@
 <#
 .SYNOPSIS
-    Simple scr
-.DESCRIPTION
-    Long description
-.EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
-.INPUTS
-    Inputs (if any)
-.OUTPUTS
-    Output (if any)
-.NOTES
-    General notes
+    Simple script for ensuring the dependencies for the build tasks are installed and working.
 #>
 #Requires -RunAsAdministrator
+Write-Host "Ensuring dependencies are installed"
 $requirements = @{
     mdt        = 'latest'
     virtualbox = 'latest'
     packer     = 'latest'
 }
 $AcceptedExitCodes = @(0,1641,3010)
+Write-Verbose "Checking Chocolatey is available"
 try
 {
+    # Cast to variable to stop superflous stdout
     $choco = Get-Command choco -ErrorAction stop
 }
 catch
@@ -30,32 +22,38 @@ catch
 }
 foreach ($requirement in $requirements.GetEnumerator())
 {
+    Write-Verbose "Checking $($requirement.key) $($requirement.value) installed"
     $InstallCheck = choco list $requirement.key --local-only -r -e
     if ($InstallCheck)
     {
+        Write-Verbose "$($requirement.key) already installed"
         $VersionCheck = $InstallCheck -replace ("^$($requiremnet.key)\|")
         $AvailableVersion = (choco list $requirement.key -r -e) -replace ("^$($requiremnet.key)\|")
     }
     else
     {
+        Write-Verbose "$($requirement.key) requires installation"
         $installargs = @('install', $requirement.key,'-y')
         if ($requirement.value -ne 'latest')
         {
             $installargs += "--version $($requirement.value)"
         }
+        Write-Debug "Install parameters:`n$($installargs)"
         $installresult = Start-Process "choco" -ArgumentList $installargs -PassThru -NoNewWindow -Wait
         if ($installresult.ExitCode -notin $AcceptedExitCodes)
         {
             throw "failed to install $($requirement.key)"
         }
+        # move on to the next
         continue
     }
     if (($requirement.value -eq 'latest') -and ($VersionCheck -ne $AvailableVersion))
     {
+        Write-Verbose "Version $VersionCheck installed, but $AvailableVersion is available, attempting to upgrade"
         $updateresult = Start-Process "choco" -ArgumentList "upgrade $($requirement.key) -y" -PassThru -NoNewWindow -Wait
         if ($updateresult.ExitCode -notin $AcceptedExitCodes)
         {
-            throw "failed to update $($requirement.key)"
+            throw "failed to upgrade $($requirement.key)"
         }
     }
 }
